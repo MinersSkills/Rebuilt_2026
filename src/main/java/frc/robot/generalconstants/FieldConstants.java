509b84d58566a751a9636fa2be66209c141178c9
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-14 10:31:24 -0300

    feat: creating field constants poses

---
+++ src/main/java/frc/robot/generalconstants/FieldConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.generalconstants;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class FieldConstants {
+    public static Pose2d HubCenter = new Pose2d(4.631, 4.069, new Rotation2d(4.631, 4.069));
+
+    public static Pose2d ScorePositionRight = new Pose2d(2.48, 2.55, new Rotation2d());
+}
