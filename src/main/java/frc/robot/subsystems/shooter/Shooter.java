b215cbd6d82aabe480dca239128eb6a9a2c403a5
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-16 10:17:30 -0300

    feat: adding the shooter starter class

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.shooter;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase{
+    private SparkMaxConfig motorShooterConfig;
+
+    private XboxController driverXbox = new XboxController(0);
+
+    public static SparkMax shooter = new SparkMax(1, MotorType.kBrushless);
+
+    public Shooter(){
+        motorShooterConfig = new SparkMaxConfig();
+
+        motorShooterConfig.idleMode(IdleMode.kBrake);
+        motorShooterConfig.inverted(false);
+    }
+
+    public void shootState(){
+        if (driverXbox.getYButton() ==  true){
+            shooter.set(1);
+        } else {
+            shooter.set(0);
+        }
+    }
+}
