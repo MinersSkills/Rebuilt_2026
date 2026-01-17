4720fc831f6e2a2bf609271c68f0906399cebf9f
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-17 13:44:46 -0300

    chore: adding the setpoints and speed to intake constants

--- src/main/java/frc/robot/generalconstants/IntakeConstants.java
+++ src/main/java/frc/robot/generalconstants/IntakeConstants.java
@@ -11,4 +11,20 @@ public final class IntakeConstants {
         public static final int MAX_VELOCITY = 3;
         public static final int MAX_ACELERATION = 4;
     }
+
+    public final class Setpoints{
+        /*
+         * Only provisory values 
+        */
+        public static final double POSITION_UP = 0.1;
+        public static final double POSITION_DOWN = 50;
+    }
+
+    public final class Speeds{
+        /*
+         * Only provisory values 
+        */
+        public static final double SPEED_INTAKE = 1;
+        public static final double SPEED_OUTAKE = -0.4;
+    }
 }
