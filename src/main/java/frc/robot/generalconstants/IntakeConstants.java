13e3d428c0865f028a39675d4bad2835644860fb
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-17 13:09:23 -0300

    feat: creating intake constants

---
+++ src/main/java/frc/robot/generalconstants/IntakeConstants.java
@@ -0,0 +1,14 @@
+package frc.robot.generalconstants;
+
+public final class IntakeConstants {
+    public final class Pid {
+        public static final double KP = 5.0;
+        public static final double KI = 0.0;
+        public static final double KD = 0.0;
+
+        public static final double TOLERANCE = 0.01;
+
+        public static final int MAX_VELOCITY = 3;
+        public static final int MAX_ACELERATION = 4;
+    }
+}
