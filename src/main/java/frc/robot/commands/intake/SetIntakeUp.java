683fed82076f7e65e10e0b2ffd8e86662265962a
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-17 14:20:35 -0300

    feat: creating a command to turn up the intake

--- src/main/java/frc/robot/commands/intake/SetIntakeUp.java
+++ src/main/java/frc/robot/commands/intake/SetIntakeUp.java
@@ -1,7 +1,27 @@
 package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
 
 public class SetIntakeUp extends Command {
+    private Intake intake;
 
+    public SetIntakeUp(Intake intake){
+        this.intake = intake;
+
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() { }
+
+    @Override
+    public void execute(){
+        intake.setIntakeUp();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
 }
