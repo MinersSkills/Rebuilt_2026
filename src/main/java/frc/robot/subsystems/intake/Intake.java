6499988c26c4fa86dbea8bee622b39eabd81ee68
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-17 12:21:56 -0300

    feat: configuring motors and encoder to intake

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,7 +1,40 @@
 package frc.robot.subsystems.intake;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
+    private SparkMaxConfig motorIntakeConfig;
+    private SparkMaxConfig motorIntakeWheelsConfig;
 
-    
+    public static SparkMax intake = new SparkMax(2, MotorType.kBrushless);
+    public static SparkMax intakeWheels = new SparkMax(3, MotorType.kBrushless);
+
+    public RelativeEncoder intakeEncoder;
+
+    public Intake(){
+        // Intake articulation 
+
+        motorIntakeConfig = new SparkMaxConfig();
+        motorIntakeConfig.idleMode(IdleMode.kBrake);
+        motorIntakeConfig.inverted(false);
+        motorIntakeConfig.encoder.positionConversionFactor(1);
+
+        intake.configure(motorIntakeConfig, null, PersistMode.kNoPersistParameters);
+
+        // Intake Wheels
+        motorIntakeWheelsConfig = new SparkMaxConfig();
+        motorIntakeWheelsConfig.idleMode(IdleMode.kBrake);
+        motorIntakeWheelsConfig.inverted(false);
+
+        intakeWheels.configure(motorIntakeConfig, null, PersistMode.kNoPersistParameters);
+
+        // Encoder
+        intakeEncoder = intake.getEncoder();
+    }
 }
