ea81aa64a9f609cbd072f742bec7fa4de4295411
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-17 14:22:35 -0300

    feat: creating a command to turn down the intake

---
+++ src/main/java/frc/robot/commands/intake/SetIntakeDown.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+public class SetIntakeDown extends Command {
+        private Intake intake;
+
+    public SetIntakeDown(Intake intake){
+        this.intake = intake;
+
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() { }
+
+    @Override
+    public void execute(){
+        intake.setIntakeDown();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+}
