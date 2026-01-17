76cda0451af9bc20117b37af967a0a48b63b5f74
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-17 14:27:41 -0300

    fix: correcting the indetation

--- src/main/java/frc/robot/commands/intake/SetIntakeDown.java
+++ src/main/java/frc/robot/commands/intake/SetIntakeDown.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.Intake;
 
 public class SetIntakeDown extends Command {
-        private Intake intake;
+    private Intake intake;
 
     public SetIntakeDown(Intake intake){
         this.intake = intake;
