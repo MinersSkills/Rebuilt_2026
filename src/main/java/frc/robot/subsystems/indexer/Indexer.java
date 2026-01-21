c5e388796d279c32625127eec04959b6a022808c
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-21 16:08:59 -0300

    feat: creating indexer class an functions

---
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.indexer;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Indexer extends SubsystemBase{
+
+    private VictorSPX indexer;
+
+    public Indexer(){
+        indexer = new VictorSPX(20);
+        indexer.setInverted(false);
+    }
+
+    public void setIndexerOn(){
+        indexer.set(ControlMode.PercentOutput, 1);
+    }
+
+    public void setIndexerOff(){
+        indexer.set(ControlMode.PercentOutput, 0);
+    }
+}
