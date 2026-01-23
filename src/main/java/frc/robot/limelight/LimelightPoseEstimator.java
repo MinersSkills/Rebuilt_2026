3e695b3a8203137efaeefeeb82873c2352b6295b
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-23 11:56:48 -0300

    feat: creating filter for each alliance

--- src/main/java/frc/robot/limelight/LimelightPoseEstimator.java
+++ src/main/java/frc/robot/limelight/LimelightPoseEstimator.java
@@ -1,6 +1,7 @@
 package frc.robot.limelight;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -11,6 +12,7 @@ public class LimelightPoseEstimator {
     private Pose2d poseMegaTag2 = new Pose2d();
     private double timestampSeconds = 0.0;
     private boolean doRejectUpdate = false;
+
 
     private Field2d field2d = new Field2d();
 
@@ -23,6 +25,17 @@ public class LimelightPoseEstimator {
         LimeLightHelpers.PoseEstimate mt2 = LimeLightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
 
         doRejectUpdate = false;
+
+        int[] idsRedAlliance = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        int[] idsBlueAlliance = {17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
+
+        var alliance = DriverStation.getAlliance();
+
+        if (alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red) {
+            LimeLightHelpers.SetFiducialIDFiltersOverride("limelight-two", idsRedAlliance);
+        }else{
+            LimeLightHelpers.SetFiducialIDFiltersOverride("limelight-two", idsBlueAlliance);
+        }
          
         // if our angular velocity is greater than 360 degrees per second, ignore vision updates
         if(mt2 == null || Math.abs(yawRate) > 360 || mt2.tagCount == 0)
