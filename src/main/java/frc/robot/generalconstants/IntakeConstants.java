9830c24b24f43832a1f0e49747ee0172bf577361
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-23 11:21:51 -0300

    feat: adjusting pid and setpoints values

--- src/main/java/frc/robot/generalconstants/IntakeConstants.java
+++ src/main/java/frc/robot/generalconstants/IntakeConstants.java
@@ -2,26 +2,20 @@ package frc.robot.generalconstants;
 
 public final class IntakeConstants {
     public final class Pid {
-        public static final double KP = 5.0;
+        public static final double KP = 0.1;
         public static final double KI = 0.0;
-        public static final double KD = 0.0;
+        public static final double KD = 0.01;
 
         public static final int MAX_VELOCITY = 3;
         public static final int MAX_ACELERATION = 4;
     }
 
     public final class Setpoints{
-        /*
-         * Only provisory values 
-        */
-        public static final double POSITION_UP = 0.1;
-        public static final double POSITION_DOWN = 50;
+        public static final double POSITION_UP = 0.3;
+        public static final double POSITION_DOWN = 4;
     }
 
     public final class Speeds{
-        /*
-         * Only provisory values 
-        */
         public static final double SPEED_INTAKE = 1;
         public static final double SPEED_OUTAKE = -0.4;
     }
