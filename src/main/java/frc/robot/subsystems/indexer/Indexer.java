322366ab194a28d8c7c570adfc1f6bcc23159b25
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-28 15:50:12 -0300

    chore: testing the indexer

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -3,15 +3,26 @@ package frc.robot.subsystems.indexer;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase{
 
     private VictorSPX indexer;
 
+    private XboxController driver = new XboxController(1);
+
     public Indexer(){
         indexer = new VictorSPX(20);
         indexer.setInverted(false);
+    }
+
+    public void setIndexerTest(){
+        if (driver.getAButtonPressed()){
+            setIndexerOn();
+        } else if(driver.getXButtonPressed()){
+            setIndexerOff();
+        }
     }
 
     public void setIndexerOn(){
