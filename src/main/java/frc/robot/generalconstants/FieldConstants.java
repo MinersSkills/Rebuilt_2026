ab1ad93e07eeb7a2ad7d67cd15eb2011032b88b7
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-01-30 16:09:19 -0300

    chore: adjusting the poses to score for better permormance of the shooter

--- src/main/java/frc/robot/generalconstants/FieldConstants.java
+++ src/main/java/frc/robot/generalconstants/FieldConstants.java
@@ -7,8 +7,8 @@ import edu.wpi.first.math.util.Units;
 public class FieldConstants {
     public static Pose2d HubCenter = new Pose2d(4.631, 4.069, new Rotation2d(4.631, 4.069));
 
-    public static Pose2d ScorePositionRight = new Pose2d(2.48, 2, new Rotation2d(Units.degreesToRadians(43)));
-    public static Pose2d ScorePositionLeft = new Pose2d(2.48, 6, new Rotation2d(Units.degreesToRadians(-43)));
+    public static Pose2d ScorePositionRight = new Pose2d(2.703, 2.127, new Rotation2d(Units.degreesToRadians(44.01)));
+    public static Pose2d ScorePositionLeft = new Pose2d(2.767, 5.558, new Rotation2d(Units.degreesToRadians(-38.12)));
     public static Pose2d ScorePositionCenter = new Pose2d(2.48, 4, new Rotation2d());
 
 }
