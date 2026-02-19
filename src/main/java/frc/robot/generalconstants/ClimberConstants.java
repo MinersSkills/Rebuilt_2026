13d908ba30e16727759945942b090a39654479ff
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-02-19 19:32:47 -0300

    feat: creating constants for the climber

---
+++ src/main/java/frc/robot/generalconstants/ClimberConstants.java
@@ -0,0 +1,14 @@
+package frc.robot.generalconstants;
+
+public class ClimberConstants {
+    public final static class Pid {
+        public static final double kP = 0.056;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+    }
+
+    public final static class Setpoints {
+        public static final double SETPOINT_UP = 363;
+        public static final double  SETPOINT_DOWN = 0; // -30
+    }
+}
