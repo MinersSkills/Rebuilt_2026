7bcf39b7648c9e5e8fa721b9b02c98d74a6060bf
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-02-19 19:36:05 -0300

    chore: improving intake class

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -5,25 +5,35 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generalconstants.ClimberConstants;
 
 public class Climber extends SubsystemBase{
     private SparkMaxConfig motorClimberConfig;
 
-    public static SparkMax climber = new SparkMax(1, MotorType.kBrushless);
+    XboxController driver = new XboxController(0);
+
+    public static SparkMax climber = new SparkMax(7, MotorType.kBrushless);
 
     public RelativeEncoder encoder;
 
     public Climber(){
         // Climber motor
         motorClimberConfig = new SparkMaxConfig();
-
         motorClimberConfig.idleMode(IdleMode.kBrake);
-        motorClimberConfig.inverted(false);
+        motorClimberConfig.inverted(true);
         motorClimberConfig.encoder.positionConversionFactor(1);
+
+        // PID
+        motorClimberConfig.closedLoop.pid(ClimberConstants.Pid.kP, 
+                                          ClimberConstants.Pid.kI,  
+                                          ClimberConstants.Pid.kD);
+        climber.configure(motorClimberConfig, null, PersistMode.kNoPersistParameters);
 
         climber.configure(motorClimberConfig, null, PersistMode.kNoPersistParameters);
 
@@ -32,20 +42,36 @@ public class Climber extends SubsystemBase{
     }
 
     public void setSetpoint(double setpoint){
-        motorClimberConfig.closedLoop.pid(0, 
-                                          0,  
-                                          0);
-        climber.configure(motorClimberConfig, null, PersistMode.kNoPersistParameters);
-        climber.getClosedLoopController().setSetpoint(setpoint, ControlType.kPosition);
+        climber.getClosedLoopController().setSetpoint(
+                                                            setpoint,
+                                                            ControlType.kPosition
+                                                      );
     }
 
-    public void SetClimber() { };
+    public void SetClimber() { 
+        if (driver.getRightBumper()){
+            setClimberDown();
+        } else if (driver.getRightTriggerAxis() > 0.5){
+            setClimberUp();
+        } else {
+            climber.set(0);
+        }
+    };
 
     public void setClimberUp(){
-        setSetpoint(10);
+        setSetpoint(ClimberConstants.Setpoints.SETPOINT_UP);
     }
 
     public void setClimberDown(){
-        setSetpoint(0);
+        setSetpoint(ClimberConstants.Setpoints.SETPOINT_DOWN);
+    }
+
+    public void setClimberOff(){
+        climber.set(0);
+    }
+
+    public void dashboard(){
+        SmartDashboard.putNumber("Encoder Climber", encoder.getPosition());
+        SmartDashboard.putNumber("Climber setpoint", ClimberConstants.Setpoints.SETPOINT_UP);
     }
 }
