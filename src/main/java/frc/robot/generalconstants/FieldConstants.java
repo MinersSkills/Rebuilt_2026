bfa5a124407734d1cab1f7f131daaea9d8a5488d
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-02-20 11:16:39 -0300

    feat: adding the passing poses to FieldConstants

--- src/main/java/frc/robot/generalconstants/FieldConstants.java
+++ src/main/java/frc/robot/generalconstants/FieldConstants.java
@@ -7,8 +7,16 @@ import edu.wpi.first.math.util.Units;
 public class FieldConstants {
     public static Pose2d HubCenter = new Pose2d(4.631, 4.069, new Rotation2d(4.631, 4.069));
 
+    /*
+     * Score positions
+     */
     public static Pose2d ScorePositionRight = new Pose2d(2.703, 2.127, new Rotation2d(Units.degreesToRadians(44.01)));
     public static Pose2d ScorePositionLeft = new Pose2d(2.767, 5.558, new Rotation2d(Units.degreesToRadians(-38.12)));
     public static Pose2d ScorePositionCenter = new Pose2d(2.48, 4, new Rotation2d());
 
+    /*
+     * Pass positions
+     */
+    public static Pose2d LeftPassPosition = new Pose2d(6, 5.4, new Rotation2d(Units.degreesToRadians(180)));
+    public static Pose2d RightPassPosition = new Pose2d(6, 2.8, new Rotation2d(Units.degreesToRadians(180)));
 }
