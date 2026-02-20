7a3a01ea88648b9a8c1d5aaa65ec92e8e9920946
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-02-20 17:55:32 -0300

    feat: adding new necessary pass poses

--- src/main/java/frc/robot/generalconstants/FieldConstants.java
+++ src/main/java/frc/robot/generalconstants/FieldConstants.java
@@ -19,4 +19,7 @@ public class FieldConstants {
      */
     public static Pose2d LeftPassPosition = new Pose2d(6, 5.4, new Rotation2d(Units.degreesToRadians(180)));
     public static Pose2d RightPassPosition = new Pose2d(6, 2.8, new Rotation2d(Units.degreesToRadians(180)));
+
+    public static Pose2d LeftPassPositionRed = new Pose2d(11, 5.4, new Rotation2d(Units.degreesToRadians(0)));
+    public static Pose2d RightPassPositionRed = new Pose2d(11, 2.8, new Rotation2d(Units.degreesToRadians(0)));
 }
