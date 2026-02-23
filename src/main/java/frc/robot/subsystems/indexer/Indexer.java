ee092b31439eacbbac4fbe8816b11cb92e0a228e
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-02-23 09:41:32 -0300

    chore: removing unused function from indexer class

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -2,28 +2,16 @@ package frc.robot.subsystems.indexer;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase{
 
     private VictorSPX indexer;
 
-    private XboxController driver = new XboxController(0);
-
     public Indexer(){
         indexer = new VictorSPX(6);
 
         indexer.setInverted(false);
-    }
-
-    public void setIndexerTest(){
-        if (driver.getYButton()){
-            indexer.set(ControlMode.PercentOutput, 0.5);
-        } else{
-            indexer.set(ControlMode.PercentOutput, 0);
-        }
     }
 
     public void setIndexerOn(){
