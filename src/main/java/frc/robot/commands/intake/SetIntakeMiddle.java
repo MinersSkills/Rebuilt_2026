b30053283db012e3c825500a2f71981b8354a646
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-03-04 12:30:25 -0300

    feat: creating a command to set intake to middle position

---
+++ src/main/java/frc/robot/commands/intake/SetIntakeMiddle.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.generalconstants.IntakeConstants;
+import frc.robot.subsystems.intake.Intake;
+
+public class SetIntakeMiddle extends Command {
+    Intake intake;
+
+    public SetIntakeMiddle(Intake intake) {
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        intake.setIntakeMidle();
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (intake.intakeEncoder.getPosition() <= IntakeConstants.Setpoints.POSITION_MIDDLE + 0.5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
